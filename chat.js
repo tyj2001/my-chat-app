e5094b03592e5f9164ee00c7a27f8d02d6cb1e9b
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-07-17 04:35:04 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,11 +1,17 @@
+// Initialize the conversation history
+var conversationHistory = [
+    {role: 'system', content: 'You are a helpful assistant.'}
+];
+
 function sendMessage() {
     var inputBox = document.getElementById('inputBox');
     var message = inputBox.value;
     inputBox.value = '';
 
-    // Add the user's message to the chat box
+    // Add the user's message to the chat box and the history
     var chatbox = document.getElementById('chatbox');
     chatbox.innerHTML += '<p><b>You:</b> ' + message + '</p>';
+    conversationHistory.push({role: 'user', content: message});
 
     // Send the message to the server
     fetch('/api/chat', {
@@ -14,14 +20,16 @@ function sendMessage() {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            messages: [{ role: "user", content: message }]
+            messages: conversationHistory
         }),
     })
     .then(response => response.json())
     .then(data => {
-        // Add the assistant's message to the chat box
-        chatbox.innerHTML += '<p><b>Assistant:</b> ' + data['choices'][0]['message']['content'] + '</p>';
+        // Add the assistant's message to the chat box and the history
+        var assistantMessage = data['choices'][0]['message']['content'];
+        chatbox.innerHTML += '<p><b>Assistant:</b> ' + assistantMessage + '</p>';
         chatbox.scrollTop = chatbox.scrollHeight;
+        conversationHistory.push({role: 'assistant', content: assistantMessage});
     });
 }
 
@@ -30,3 +38,4 @@ document.getElementById('inputBox').addEventListener('keydown', function(e) {
         sendMessage();
     }
 });
+
