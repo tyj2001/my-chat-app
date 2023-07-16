1f706219275fa29252bd3ab9f44584cbe482d0ee
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-07-17 02:11:42 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.post('/chat', async (req, res) => {
+  const userInput = req.body.text;
+
+  try {
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      prompt: userInput,
+      max_tokens: 100,
+    }, {
+        headers: {
+          'Authorization': `Bearer YOUR_PURGPT_API_KEY`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    const generatedText = response.data.choices[0].text.trim()
+
+    res.json({ text: generatedText });
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Something went wrong while fetching the AI response');
+  }
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
