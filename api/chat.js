029c9a4e6cf85ab8e912ec1af87bb17b7ea0efe1
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-07-17 03:25:09 +0800

    Create chat.js

---
+++ api/chat.js
@@ -0,0 +1,18 @@
+module.exports = async (req, res) => {
+    const fetch = require('node-fetch');
+
+    const response = await fetch('https://purgpt.xyz/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            messages: req.body.messages,
+            key: process.env.API_KEY,
+            model: 'gpt-3.5-turbo'
+        }),
+    });
+
+    const data = await response.json();
+    res.json(data);
+}
